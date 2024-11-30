73656e3ff36b84ecd8a81375315f5907084eb0a2
Walther0296 <sau100@inbox.ru>
2024-11-30 13:45:51 +0300

    Add mydocumentation

---
+++ mydocumentation/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.gb.saushkin","l":"Decorator"},{"p":"ru.gb.saushkin","l":"Main"},{"p":"ru.gb.saushkin","l":"OtherClass"}];updateSearchResults();
