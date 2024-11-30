ab91f5d59356ac49a2cb5d250d6ec5cc8edcd02a
Walther0296 <sau100@inbox.ru>
2024-11-30 12:53:58 +0300

    Start make code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
+import ru.gb.saushkin.OtherClass;
+import ru.gb.saushkin.Decorator;
 
+/**
+ * Основной класс приложения. Здесь мы можем описать
+ * его основное назначение и способы взаимодействия с ним.
+ * */
 public class Main {
+
+    /**
+     * Точка входа в программу. С неё всегда всё начинается.
+     *
+     * @param args стандартные аргументы командной строки
+     * */
+
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        /**
+         * тут не видно
+         *
+         * */
+        // --------------------------------------------
+        int result = OtherClass.add(5, 2);
+        System.out.println(Decorator.decorate(result));
+        result = OtherClass.sub(5, 2);
+        System.out.println(Decorator.decorate(result));
+        result = OtherClass.mul(5, 2);
+        System.out.println(Decorator.decorate(result));
+        result = OtherClass.div(5, 2);
+        System.out.println(Decorator.decorate(result));
+
+        //---------------------------------------------
     }
 }
